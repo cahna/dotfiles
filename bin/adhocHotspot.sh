93b2a5a4e325057a394ce67197b4d6a2d5a859a0
Conor Heine <conor.heine@gmail.com>
2014-05-11 11:43:10 -0700

    added some networking scripts

--- bin/adhocHotspot.sh
+++ bin/adhocHotspot.sh
@@ -1,1 +1,7 @@
-sudo ./create_ap/create_ap -c 7 -w 2 wlp3s0 enp0s25 <networkname> <password>
+#!/bin/bash
+usage="USAGE: sudo $0 -c 7 -w 2 wlp3s0 enp0s25 <networkname> <password>"
+if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+  echo $usage
+else
+  sudo ./create_ap/create_ap -c 7 -w 2 wlp3s0 enp0s25 "$1" "$2"
+fi
--- bin/public_ip.sh
+++ bin/public_ip.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 url=http://jsonip.com/
+url2=http://ifconfig.me/all.json
 ua='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36'
 accept='text/json,text/*'
 alang='en-US'
@@ -8,6 +9,9 @@ cache='max-age=0'
 conn='keep-alive'
 refer='http://stackoverflow.com/questions/11658490/grab-ip-address-from-gooogle-via-json-export'
 
-curl -f -s -e "$refer" -A "$ua" -H "Accept: $accept" -H "Accept-Language: $alang" -H "DNT: 1" $url \
-  | jq -r '.ip'
-
+curl -f -s \
+  -e "$refer" \
+  -A "$ua" \
+  -H "Accept: $accept" \
+  -H "Accept-Language: $alang" \
+  -H "DNT: 1" $url | jq -r '.ip' || curl -s "$url2" | jq -r '.ip_addr'
