53a5a8b8ba9f88e6e6d4dc44fdcf863423a54fe6
Conor Heine <conor.heine@gmail.com>
2021-01-19 15:07:49 -0800

    Add more xubuntu setup

--- setup-xubuntu-20.sh
+++ setup-xubuntu-20.sh
@@ -6,7 +6,7 @@ dir_script="$(pwd)"
 dir_3p="$HOME/code/3p"
 downloads="$HOME/Downloads"
 
-sudo apt-get remove -y xubuntu-desktop xfdesktop4
+sudo apt-get remove -y xubuntu-desktop xfdesktop4 xfce4-panel
 
 sudo apt-get install -y \
     git \
@@ -17,12 +17,16 @@ sudo apt-get install -y \
     conky-all \
     tmux \
     zsh \
-    fonts-powerline \
+    xsel \
+    fonts-powerline fonts-font-awesome \
     xbacklight \
     pulseaudio-utils \
     feh \
     thunar thunar-archive-plugin thunar-volman thunar-media-tags-plugin thunar-vcs-plugin \
-    zathura zathura-pdf-poppler
+    zathura zathura-pdf-poppler \
+    jq \
+    htop \
+    python3 python3-pip
 
 
 sudo apt-get autoremove -y
@@ -52,6 +56,34 @@ cp -nr "$dir_3p/dotfiles/vim" "$HOME/.vim"
 cp -n "$dir_3p/dotfiles/vimrc" "$HOME/.vimrc"
 cp -n "$dir_3p/dotfiles/xinitrc" "$HOME/.xinitrc"
 cp -n "$dir_3p/dotfiles/xbindkeysrc" "$HOME/.xbindkeysrc"
+
+##
+# Setup AwesomeWM
+##
+sudo xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -s "awesome" -a
+
+##
+# Install VSCodium
+##
+if [ -z "$(which codium)" ]; then
+    wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
+    echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
+    sudo apt update -y
+    sudo apt install -y codium
+fi
+
+##
+# Install Docker
+##
+if [ -z "$(which docker)" ]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository \
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable"
+    sudo apt update -y
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
+fi
 
 echo "DONE!"
 
